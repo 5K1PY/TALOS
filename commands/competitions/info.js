58346289ee3a18a058eda1fabb4da30a45e7b7a2
Daniel Skýpala <daniel@honza.info>
2020-11-11 16:57:48 +0100

    Info now takes multiple competitions
    Info now can take multiple competitions as well as --all

--- commands/competitions/info.js
+++ commands/competitions/info.js
@@ -11,9 +11,10 @@ module.exports = class Info extends Command {
             description: 'Sends info for competition',
             args: [
                 {
-                    key: 'competitionName',
-                    prompt: 'For what competiton would you like info?',
+                    key: 'competitionNames',
+                    prompt: 'For what competitons would you like info?',
                     type: 'string',
+                    parse: arg => arg.split(" ")
                 }
             ],
             userPermissions: [
@@ -22,14 +23,27 @@ module.exports = class Info extends Command {
 		});
     }
 
-    run (message, {competitionName}) {
+    run (message, {competitionNames}) {
         const competitions = JSON.parse(fs.readFileSync(`./data/${message.guild.id}/competitions/competitions_data.json`, 'utf-8'));
-        let competition = competitions.find(comp => comp.name === competitionName);
         let messageText = `<@${message.author.id}> `;
-        if (competition === undefined) {
-            messageText += 'Unknown competition.';
+        let register;
+        if (competitionNames.length === 1 && (competitionNames[0].toLowerCase() === "--all" || competitionNames[0].toLowerCase() === "-a")) {
+            register = competitions;
         } else {
-            messageText += `Here's web for ${competition.category.toLowerCase()} competiton ${competition.name} ${competition.web}. ${competition.deadline != undefined ? ` Next deadline is ${competition.deadline}`: ''}`;
+            register = competitionNames.map(finding => competitions.find(comp => comp.name.toLowerCase() === finding.toLowerCase()));
+        }
+        if (register.includes(undefined)) {
+            messageText += "Unknown competitions:"
+            for (var i=0; i<competitionNames.length; i++) {
+                if (register[i] === undefined) {
+                    messageText += ` ${competitionNames[i]}` + (i===competitionNames.length-1 ? '':',')
+                }
+            }
+        } else {
+            messageText += register.length === 1 ? ' ' : '\n';
+            register.forEach(comp => {
+                messageText += `Here's web for ${comp.category.toLowerCase()} competiton ${comp.name} ${comp.web}. ${comp.deadline != undefined ? ` Next deadline is ${comp.deadline}`: ''}\n`;
+            });
         }
         message.say(messageText);
     }
